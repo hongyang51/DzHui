b21960aebf68dd0fd0a7d70f20ee69b1e5a184de
Your Name <you@example.com>
2015-10-13 19:48:06 +0800

    no message

--- app/src/main/java/com/lanou3g/mydazahui/Activity/PatchWorkOneActivity.java
+++ app/src/main/java/com/lanou3g/mydazahui/Activity/PatchWorkOneActivity.java
@@ -1,10 +1,15 @@
 package com.lanou3g.mydazahui.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.view.KeyEvent;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.lanou3g.mydazahui.R;
 import com.lanou3g.mydazahui.base.MainActivity;
@@ -17,12 +22,17 @@ import cn.jpush.android.api.JPushInterface;
  */
 public class PatchWorkOneActivity extends MainActivity implements View.OnClickListener {
     private ImageView food_image,read_image;
+    private View view_custom;
+    private Context mContext;
+    private AlertDialog alert = null;
+    private AlertDialog.Builder builder = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initFind();
+
+        initView();
         initData();
     }
 
@@ -31,16 +41,49 @@ public class PatchWorkOneActivity extends MainActivity implements View.OnClickLi
         read_image.setOnClickListener(this);
     }
 
-    private void initFind() {
+    private void initView() {
+        mContext = PatchWorkOneActivity.this;
+        //初始化Builder
+        builder = new AlertDialog.Builder(mContext);
         food_image = (ImageView) findViewById(R.id.food_image);
         read_image = (ImageView) findViewById(R.id.read_image);
+        //加载自定义的那个View,同时设置下
+        final LayoutInflater inflater = PatchWorkOneActivity.this.getLayoutInflater();
+        view_custom = inflater.inflate(R.layout.view_dialog_custom, null,false);
+        builder.setView(view_custom);
+        builder.setCancelable(false);
+        alert = builder.create();
+        view_custom.findViewById(R.id.btn_cancle).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alert.dismiss();
+            }
+        });
+        view_custom.findViewById(R.id.btn_blog).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "访问博客", Toast.LENGTH_SHORT).show();
+                Uri uri = Uri.parse("http://blog.csdn.net/wooder111");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                alert.dismiss();
+            }
+        });
+
+        view_custom.findViewById(R.id.btn_close).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "关闭", Toast.LENGTH_SHORT).show();
+                alert.dismiss();
+            }
+        });
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.food_image:
-
+                alert.show();
             break;
             case R.id.read_image:
                 boolean userGuid = SharedPreferUtil.getBoolean(this, "is_user_guide", false);
